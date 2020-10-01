966a50e665d2098570885466e3a69bbf713a886d
Nick Halstead <hikahooma@gmail.com>
2020-10-01 13:40:27 +0100

    comment out theme.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ import '../sass/index.sass'
 import initShowcase from './initShowcase';
 import initOffcanvasMenu from './initOffcanvasMenu';
 import initSlider from './initSlider';
-import theme from './theme';
+
 
 initShowcase();
 initOffcanvasMenu();
