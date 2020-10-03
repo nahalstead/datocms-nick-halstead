9adbb09c826150754a931c33accf00ab00ad3091
Nick Halstead <hikahooma@gmail.com>
2020-10-03 15:20:15 +0100

    style mobile menu and add toggle class for thumbnails

--- assets/js/initSlider.js
+++ assets/js/initSlider.js
@@ -78,9 +78,35 @@ $(window).load(function() {
     multipleKeyboard    : true,
 
     start: function(slider) {
+    var e = $(".pagination"),
+    t = $(".gallery-navigation"),
+    a = t.find(".navigation"),
+    s = !1;
+    $(".open-close-thumbs").on("click", function(t, i) {
+                                t.preventDefault(), i = void 0 === i || i;
+                                var l = parseInt(e.css("left"), 10),
+                                    r = $(".flexslider").position().top;
+                                i && (0 !== l || e.is(":animated") ? e.is(":animated") || ($(".flexslider").stop(!0, !0).animate({
+                                    opacity: 0
+                                }, n), e.css({
+                                    left: 0,
+                                    top: r
+                                }).stop(!0, !0).animate({
+                                    opacity: 1
+                                }, n), a.hide(), $(this).removeClass("icon-grid").addClass("icon-close"), s = !s) : ($(".flexslider").stop(!0, !0).animate({
+                                    opacity: 1
+                                }, n), e.stop(!0, !0).animate({
+                                    opacity: 0
+                                }, n, function() {
+                                    e.css({
+                                        left: "-100%",
+                                        top: "-9000em"
+                                    })
+                                }), o.hide(), a.show(), $(this).removeClass("icon-close").addClass("icon-grid"), s = !s))
+                            })
     $(document).on("keydown", function(e) {
         ("39" != e.which || s || l.next(e), "37" != e.which || s || l.prev(e), "27" == e.which && "1" == $(".pagination").css("opacity") && (e.preventDefault(), $(".open-close-thumbs").trigger("click")))
-        }), $(".next").on("click", l.next), $(".prev").on("click", l.prev), $(".thumb_container .thumb_item").eq(i).find("a").trigger("click", [!1]), $(".load_iframe").addClass("icon-video").parent().append('<div class="layer"></div>')
+        }), $(".next").on("click", l.next), $(".prev").on("click", l.prev), $(".thumb_container .thumb_item").eq(i).find("a").trigger("click", [!1])
     }
   });
 });
