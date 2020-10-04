845f3638e4c1b9d7d3195dc39d368da578756afa
Nick Halstead <hikahooma@gmail.com>
2020-10-04 18:27:48 +0100

    rework slider

--- assets/js/initSlider.js
+++ assets/js/initSlider.js
@@ -4,33 +4,34 @@ export default function initSlider() {
 
         // initiate slider
         var main = function() {
-            $('.next').add('.flexslider img').click(function() {
-                var currentSlide = $('.flexslider .active');
+            $('.assets_container').first().addClass('flex-active-slide');
+            $('.next').add('.flexslider .image-wrapper').click(function() {
+                var currentSlide = $('.flexslider .flex-active-slide');
                 var nextSlide = currentSlide.next();
 
                 if (nextSlide.length == 0) {
-                    nextSlide = $('.flexslider img').first();
+                    nextSlide = $('.asset .image').first();
                 }
 
-                currentSlide.fadeOut(100).removeClass('active');
-                nextSlide.fadeIn(100).addClass('active');
+                currentSlide.fadeOut(100).removeClass('flex-active-slide');
+                nextSlide.fadeIn(100).addClass('flex-active-slide');
             });
             $('.prev').click(function() {
-                var currentSlide = $('.active');
+                var currentSlide = $('.flex-active-slide');
                 var prevSlide = currentSlide.prev();
 
                 if (prevSlide.length == 0) {
-                    prevSlide = $('.flexslider img').last();
+                    prevSlide = $('.asset .image').last();
                 }
 
-                currentSlide.fadeOut(100).removeClass('active');
-                prevSlide.fadeIn(100).addClass('active');
+                currentSlide.fadeOut(100).removeClass('flex-active-slide');
+                prevSlide.fadeIn(100).addClass('flex-active-slide');
             });
 
             // initiate thumbnail click function to find corresponding slide
-            $('.slideshow_thumbnails > img').click(function() {
+            $('.pagination > img').click(function() {
                 // toggle inactive all images in slideshow 
-                $('.flexslider img').toggleClass('inactive');
+                $('.asset .image').toggleClass('inactive');
 
                 // hide thumbnails
                 $('.slideshow_thumbnails').toggleClass('hide').removeClass('show');
@@ -39,11 +40,11 @@ export default function initSlider() {
                 $('.flexslider').toggleClass('show').removeClass('hide');
 
                 //Define current slide
-                var currentSlide = $('.flexslider .active');
-                currentSlide.toggleClass('active');
+                var currentSlide = $('.flex-active-slide .image');
+                currentSlide.toggleClass('flex-active-slide');
 
                 //Define current slide integer
-                var currentSlideInt = $('.flexslider .active').data("id");
+                var currentSlideInt = $('.flexslider .flex-active-slide').data("id");
 
                 //Grab data-id value from thumbnail;
                 var slideTo = $(this).data("id")
@@ -52,10 +53,10 @@ export default function initSlider() {
                 var slideToInt = parseInt(slideTo);
 
                 //Define "selected" slide to travel to
-                var slideSelected = $('.flexslider .image-' + slideToInt);
+                var slideSelected = $('.asset .image-' + slideToInt);
 
                 //move flexslider to the correct slide
-                slideSelected.fadeIn(100).toggleClass('active');
+                slideSelected.fadeIn(100).toggleClass('flex-active-slide');
             })
         };
 
@@ -65,21 +66,21 @@ export default function initSlider() {
         // Count slides and thumbnails and assign to data-id for each element
         $(document).ready(function() {
             var i = 0;
-            $(".flexslider img").each(function() {
+            $(".asset .image").each(function() {
                 $(this).attr("data-id", +i);
                 i++;
             });
         });
         $(document).ready(function() {
             var i = 0;
-            $(".flexslider img").each(function() {
+            $(".asset .image").each(function() {
                 $(this).addClass(("image-") + (+i));
                 i++;
             });
         });
         $(document).ready(function() {
             var i = 0;
-            $(".slideshow_thumbnails img").each(function() {
+            $(".pagination > img").each(function() {
                 $(this).attr("data-id", +i);
                 i++;
             });
@@ -88,13 +89,13 @@ export default function initSlider() {
         // Thumbnail toggle function
         $(document).ready(function() {
             var count = $('.flexslider').children().length;
-            var currentSlide = $('.flexslider .active');
+            var currentSlide = $('.flexslider .flex-active-slide');
 
             $('.open-close-thumbs').click(function() {
-                $('.thumb_container').toggleClass('show');
+                $('.pagination').toggleClass('show');
                 $('.flexslider').toggleClass('hide');
-                $('.flexslider img').toggleClass('inactive');
-                currentSlide.toggleClass('active');
+                $('.asset .image').toggleClass('inactive');
+                currentSlide.toggleClass('flex-active-slide');
             })
         });
 
@@ -103,23 +104,23 @@ export default function initSlider() {
         function getWidth() {
             var e = null;
             e = document.getElementsByTagName('img');
-            var imageWidth = $('.flexslider img').get(0).naturalWidth;
-            var imageHeight = $('.flexslider img').get(0).naturalHeight;
-            var t = $('.flexslider img').get(0).naturalHeight;
-            var n = $('.flexslider img').get(0).naturalWidth;
+            var imageWidth = $('.flexslider > img').get(0).naturalWidth;
+            var imageHeight = $('.flexslider > img').get(0).naturalHeight;
+            var t = $('.flexslider > img').get(0).naturalHeight;
+            var n = $('.flexslider > img').get(0).naturalWidth;
             var r = ($(window).height() - 20) / t;
             var i = $(window).width() / n;
             return r < i ? r / i * 100 + "%" : "100%";
         }
 
         $(document).ready(function() {
-            $('.flexslider img').css("max-width", getWidth())
-            $('.flexslider img').css({
+            $('.flexslider > img').css("max-width", getWidth())
+            $('.flexslider > img').css({
                 "display": "block"
             })
         });
         $(window).resize(function() {
-            $(".flexslider img").css("max-width", getWidth())
+            $(".flexslider > img").css("max-width", getWidth())
         });
 
     });
