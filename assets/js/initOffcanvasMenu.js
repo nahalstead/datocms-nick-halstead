11982d5bdaed2ad3f7dee4905c4d4a8bedfd1109
Nick Halstead <hikahooma@gmail.com>
2020-10-11 16:36:20 +0100

    mobile menu update

--- assets/js/initOffcanvasMenu.js
+++ assets/js/initOffcanvasMenu.js
@@ -1,9 +1,11 @@
 export default function initOffcanvasMenu() {
-  const hamburger = document.querySelector('.hamburger');
+  const hamburger = document.querySelector('.mobile-menu-open-button');
   if (!hamburger) return;
 
   hamburger.addEventListener('click', (e) => {
     e.preventDefault();
     document.querySelector('.container').classList.toggle('is-open');
+    document.querySelector('.mobile-menu').classList.toggle('active');
+    document.querySelector('.gallery').classList.toggle('locked');
   });
 }
