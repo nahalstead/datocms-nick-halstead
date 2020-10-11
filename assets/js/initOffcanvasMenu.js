8840e120eb48a2cfe39eb2d06e42efe05dab4969
Nick Halstead <hikahooma@gmail.com>
2020-10-11 16:47:16 +0100

    mobile menu functionality

--- assets/js/initOffcanvasMenu.js
+++ assets/js/initOffcanvasMenu.js
@@ -1,8 +1,15 @@
 export default function initOffcanvasMenu() {
   const hamburger = document.querySelector('.mobile-menu-open-button');
   if (!hamburger) return;
+  const close = document.querySelector('.mobile-menu-close-button');
 
   hamburger.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('.container').classList.toggle('is-open');
+    document.querySelector('.mobile-menu').classList.toggle('active');
+    document.querySelector('.gallery').classList.toggle('locked');
+  });
+  close.addEventListener('click', (e) => {
     e.preventDefault();
     document.querySelector('.container').classList.toggle('is-open');
     document.querySelector('.mobile-menu').classList.toggle('active');
